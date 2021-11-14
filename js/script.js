88dbcfa6c9fafe86a089c496cea376ca9165690b
jordi <jordi.lanau@yahoo.com>
2021-11-14 20:14:18 +0000

    Make start time in loadPlanner() constant

--- js/script.js
+++ js/script.js
@@ -10,25 +10,25 @@ function displayCurrentTime() {
 }
 
 function loadPlanner() {
-  let start = moment('08:00:00', 'hh:mm:ss');
-  const end = moment('23:00:00', 'hh:mm:ss');
-  const diff = end.diff(start, 'hours');
+  const startTime = moment('08:00:00', 'hh:mm:ss');
+  const endTime = moment('23:00:00', 'hh:mm:ss');
+  const diff = endTime.diff(startTime, 'hours');
+  let blockTime = startTime.clone();
   const timeblocks = [...Array(diff)]
     .map((block) => {
       block = `
         <div class="row time-block">
-          <div class="col-1 hour">${start.format('HH:mm')}</div>
+          <div class="col-1 hour">${blockTime.format('HH:mm')}</div>
           <textarea class="col-10">some text</textarea>
           <button class="saveBtn col-1">
             <i class="fas fa-save"></i>
           </button>
         </div>
       `;
-      start = start.add(1, 'hours');
+      blockTime = blockTime.add(1, 'hours');
       return block;
     })
     .join('');
 
   $('.jumbotron + .container').append(timeblocks);
-  console.log($('.jumbotron + .container'));
 }
